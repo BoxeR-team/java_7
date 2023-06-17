b04053013f4a946e76c9fbb0125c0697b59edba3
BoxeR-team <79333790+BoxeR-team@users.noreply.github.com>
2023-06-17 05:26:08 +0200

    Strong job

---
+++ HomeWork_1/src/main/java/Task1/Task1.java
@@ -0,0 +1,27 @@
+package Task1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter symbols: \n");
+        String str = scanner.nextLine();
+        char[] mass = str.toCharArray();
+        int sum = 0;
+
+        for (int i = 0; i < mass.length; i++) {
+            Boolean isDigit = Character.isDigit(mass[i]);
+            if (isDigit) {
+                int num = Character.getNumericValue(mass[i]);
+                sum = sum + num;
+
+            }
+        }
+        System.out.println(sum);
+        if (sum ==sum){
+            System.out.println("Done");
+        }
+    }
+}
