ddec386a4f70fec14eeac0315985ec863dc699ed
Volodymyr <79333790+BoxeR-team@users.noreply.github.com>
2023-06-22 11:24:50 +0200

    Delete homeWork1 directory

--- homeWork1/task1.java
+++
@@ -1,8 +0,0 @@
-package homeWork1;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class task1 {
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-}
